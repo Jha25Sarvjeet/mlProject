89d516e0fbfd3de0d07b36a8c34f43d1ccdc3eda
Jha24Sarvjeet <jha.sarvjeet1@gmail.com>
2025-02-18 15:37:20 +0530

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"log",LOG_FILE)
+log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
@@ -15,5 +15,5 @@ logging.basicConfig(
 
 )
 
-if __name__=="__main__":
-    logging.info("Logging has started")
+# if __name__=="__main__":
+#     logging.info("Logging has started")
